e561b84409ff8baca2fa4cce23ad92d28394c1d5
Diego Filgueiras <diego.filgueiras.084@ufrn.edu.br>
2023-10-05 10:47:27 -0300

    fix: adiciona métodos acessores do dto de proposta de desenho

--- src/main/java/com/eliasfs06/tinktime/model/dto/PropostaDesenhoDTO.java
+++ src/main/java/com/eliasfs06/tinktime/model/dto/PropostaDesenhoDTO.java
@@ -21,4 +21,36 @@ public class PropostaDesenhoDTO {
         this.propostaOrcamento = new PropostaOrcamentoDTO(propostaDesenho.getPropostaOrcamento());
         this.statusAprovacao = propostaDesenho.getStatusAprovacao().getDenominacao();
     }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public byte[] getDesenho() {
+        return desenho;
+    }
+
+    public void setDesenho(byte[] desenho) {
+        this.desenho = desenho;
+    }
+
+    public PropostaOrcamentoDTO getPropostaOrcamento() {
+        return propostaOrcamento;
+    }
+
+    public void setPropostaOrcamento(PropostaOrcamentoDTO propostaOrcamento) {
+        this.propostaOrcamento = propostaOrcamento;
+    }
+
+    public String getStatusAprovacao() {
+        return statusAprovacao;
+    }
+
+    public void setStatusAprovacao(String statusAprovacao) {
+        this.statusAprovacao = statusAprovacao;
+    }
 }
