ed754557eefec4d54be683ec5e55e22d45e2f639
Diego Filgueiras <diego.filgueiras.084@ufrn.edu.br>
2023-10-09 02:09:43 -0300

    feat: sobrescreve método de cadastro de proposta de desenho na camada de controle

--- src/main/java/com/eliasfs06/tinktime/restController/PropostaDesenhoRestController.java
+++ src/main/java/com/eliasfs06/tinktime/restController/PropostaDesenhoRestController.java
@@ -1,7 +1,10 @@
 package com.eliasfs06.tinktime.restController;
 
+import com.eliasfs06.tinktime.exceptionsHandler.BusinessException;
 import com.eliasfs06.tinktime.model.PropostaDesenho;
+import com.eliasfs06.tinktime.model.dto.PropostaDesenhoDTO;
 import com.eliasfs06.tinktime.repository.GenericRepository;
+import com.eliasfs06.tinktime.service.PropostaDesenhoService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -12,13 +15,21 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("proposta-desenho")
 public class PropostaDesenhoRestController extends GenericRestController<PropostaDesenho> {
 
-    public PropostaDesenhoRestController(GenericRepository<PropostaDesenho> repository) {
-        super(repository);
+    private final PropostaDesenhoService propostaDesenhoService;
+
+    public PropostaDesenhoRestController(GenericRepository<PropostaDesenho> propostaDesenhoGenericRepository,
+                                         PropostaDesenhoService propostaDesenhoService) {
+        super(propostaDesenhoGenericRepository);
+        this.propostaDesenhoService = propostaDesenhoService;
     }
 
     @Override
     @PostMapping("")
     public ResponseEntity<PropostaDesenho> create(@RequestBody PropostaDesenho created) {
-        return super.create(created);
+        try {
+            return ResponseEntity.ok(propostaDesenhoService.create(new PropostaDesenhoDTO(created)));
+        } catch (BusinessException e) {
+            return ResponseEntity.badRequest().build();
+        }
     }
 }
