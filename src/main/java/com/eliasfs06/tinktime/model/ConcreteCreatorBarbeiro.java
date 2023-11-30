ada795e8f074e45a24f6d357093d0c34436163f4
Isaque <isaque.bm9@gmail.com>
2023-11-30 11:20:24 -0300

    remoção modelos nao usados

--- src/main/java/com/eliasfs06/tinktime/model/ConcreteCreatorBarbeiro.java
+++
@@ -1,8 +0,0 @@
-package com.eliasfs06.tinktime.model;
-
-public class ConcreteCreatorBarbeiro implements FuncionarioCreator {
-    @Override
-    public Funcionario createFuncionario() {
-        return new Barbeiro();
-    }
-}
