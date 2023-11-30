19422a388b470a178671047ba68032c60fa8067e
Diego Filgueiras <diego.filgueiras.084@ufrn.edu.br>
2023-11-30 20:04:34 -0300

    refactor: otimiza importação de classes

--- src/main/java/com/eliasfs06/tinktime/service/suggestionObserver/SuggestionObserver.java
+++ src/main/java/com/eliasfs06/tinktime/service/suggestionObserver/SuggestionObserver.java
@@ -2,11 +2,8 @@ package com.eliasfs06.tinktime.service.suggestionObserver;
 
 import com.eliasfs06.tinktime.model.Client;
 import com.eliasfs06.tinktime.model.Funcionario;
-import com.eliasfs06.tinktime.model.TattoStyle;
-import com.eliasfs06.tinktime.model.Tatuador;
 import com.eliasfs06.tinktime.model.enums.UnidadeFederacao;
 import com.eliasfs06.tinktime.repository.ClienteRepository;
-import com.eliasfs06.tinktime.repository.FuncionaroRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
